76be746b434fe11172c8513f3c76c24abfb44d7f
Carlos Nardo <c.cesar.nardo@gmail.com>
2021-12-01 20:49:48 -0300

    add some tests

---
+++ src/utils/tests/form-mock.ts
@@ -0,0 +1,34 @@
+import { FormFields } from "../../models/entities";
+
+const mock: FormFields = {
+  question1: 1,
+  question2: 1,
+  question3: 1,
+  question4: 1,
+  question5: 1,
+  coverage1: "sim",
+  coverage2: "sim",
+  coverage3: "sim",
+  coverage4: "sim",
+  coverage5: "sim",
+  coverage6: "sim",
+  coverage7: "sim",
+  coverage8: "sim",
+  coverage9: "sim",
+  coverage10: "sim",
+  coverage11: "sim",
+  coverage12: "sim",
+  coverage13: "sim",
+  name: "",
+  cpf: "",
+  birthdate: "",
+  cep: "",
+  address: "",
+  number: "",
+  complement: "",
+  neighborhood: "",
+  city: "",
+  state: "",
+};
+
+export default mock;
