2fd7caccb41cfca14e74da95185d4989f2ddc2b1
Carlos Nardo <c.cesar.nardo@gmail.com>
2021-12-01 18:26:24 -0300

    adds CEP api call
    (cherry picked from commit 3e94db8ccaf441e8f8f901d0ebbc98f754b204fa)

--- src/pages/api/find-cep.ts
+++ src/pages/api/find-cep.ts
@@ -3,9 +3,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 const FindCepAPI = async (req: NextApiRequest, res: NextApiResponse) => {
   const { cep } = req.body;
 
-  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
 
-  return await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 export default FindCepAPI;
